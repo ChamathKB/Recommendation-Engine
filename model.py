89e0ee8d708ea709643ae8d13d016a4950a547d4
ChamathKB <amsmckbattanayaka@std.appsc.sab.ac.lk>
2023-01-25 00:34:52 +0530

    adding model module

---
+++ model.py
@@ -0,0 +1,55 @@
+from pyspark.sql import SparkSession
+from pyspark import SparkContext
+from pyspark.sql.functions import col, explode
+
+from pyspark.ml.evaluation import RegressionEvaluator
+from pyspark.ml.recommendation import ALS
+from pyspark.ml.tuning import ParamGridBuilder, CrossValidator
+
+
+class Model():
+
+    def __init__(self, data):
+        self.data = data
+
+    def model(self):
+        train, test = self.data.randomSplit([0.8, 0.2], seed=1)
+
+        als = ALS(userCol='userId',
+                itemCol='movieId',
+                ratingCol='rating',
+                nonnegative=True, implicitPrefs=False, coldStartStrategy='drop')
+
+        return train, test, als
+
+class Tune():
+    def __init__(self, model):
+        self.model = model
+
+    def tune(self, ranks, regparams, metric, labelcol, predictioncol):
+        param_grid = ParamGridBuilder() \
+                    .addGrid(self.model.rank, ranks) \
+                    .addGrid(ranks.regParam, regparams) \
+                    .build()
+
+        evaluator = RegressionEvaluator(metricName=metric, labelCol=labelcol, predictionCol=predictioncol)
+
+        print("Number of models to test: ", len(param_grid))
+
+        return evaluator, param_grid
+
+        
+    def best_model(self):
+        best_model = self.model.bestModel
+
+        print(type(best_model))
+
+        print("Best model ->")
+
+        print("  Rank: ", best_model._java_obj.parent().getRank())
+
+        print("  MaxIter: ", best_model._java_obj.parent().getMaxIter())
+
+        print("  RegParam: ", best_model._java_obj.parent().getRegParam())
+
+        return best_model
