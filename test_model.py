066d8060403f3ea35aad714dc784fb3299c2599d
ChamathKB <amsmckbattanayaka@std.appsc.sab.ac.lk>
2023-02-10 21:16:14 +0530

    updated test module

--- test_model.py
+++ test_model.py
@@ -1,10 +1,11 @@
 import pytest
 from pyspark.sql import SparkSession
-from data_pipeline import sparsity
+from model import Model, Tune, Utils
 
 spark = SparkSession.builder.appName('Recommendation').getOrCreate()
 
 data = spark.read.csv('data/ratings.csv', header=True)
 
 def test_sparsity(data):
-    assert 'dataframe' in sparsity(data=data)
+    sparsity = Utils.sparsity()
+    assert type(sparsity(data)) == int
