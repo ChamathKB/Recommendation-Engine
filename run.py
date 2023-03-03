a33232e911f3bdf9b8abdb1e049a228ea7361942
ChamathKB <amsmckbattanayaka@std.appsc.sab.ac.lk>
2023-03-03 23:47:37 +0530

    refactored codebase

--- run.py
+++ run.py
@@ -7,19 +7,6 @@ model_path = '/model/model'
 
 best_model = Model.load_model('model/ALS_model/bestModel')
 
-# als = ALS(userCol='userId',
-#                 itemCol='movieId',
-#                 ratingCol='rating',
-#                 nonnegative=True, implicitPrefs=False, coldStartStrategy='drop')
-
-# ranks = [10, 50, 100, 150]
-# regparams = [.01, .05, .1, .15]
-
-# evaluator, param_grid  = Tune.tune(als, ranks, regparams, 'rmse', 'ratings', 'prediction')
-
-# predict = Model.predict(model=model, evaluator=evaluator)
-
-
 
 n_recommendations = best_model.recommendForAllUsers(10)
 n_recommendations.limit(10).show()
